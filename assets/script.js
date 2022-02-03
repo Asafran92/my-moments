6ba56c56be30f7408af37f5fe1d9904a81b185c8
unknown <avrumiesafranovitz@gmail.com>
2022-02-03 01:06:50 -0500

    added all hours and save buttons

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ var currentDay = document.querySelector("#currentDay");
 
 
 //setting up current day
-var currentDayStart = moment().format('DD-MM-YYYY');
+var currentDayStart = moment().format('MM-DD-YYYY');
     console.log(currentDayStart);
 
 currentDay = currentDayStart.textContent;
