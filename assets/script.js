49cb9da8db4d9abc8ed5b05e4e9e43636da4252d
unknown <avrumiesafranovitz@gmail.com>
2022-02-03 23:34:40 -0500

    added local storage

--- assets/script.js
+++ assets/script.js
@@ -8,3 +8,20 @@ var currentDayStart = moment().format('MM-DD-YYYY');
 currentDay = currentDayStart.textContent;
 $("#currentDay").append(currentDayStart);
 $("#currentDay").addClass("lead");
+
+//get row description into local storage
+//var saveTasks = function() {
+//    localStorage.setItem("input", JSON.stringify(input));
+//}
+//$(".saveBtn").on("click", "input", function() {
+//    saveTasks;
+//});
+
+//var saveTasksObject = {"p": "input"};
+//localStorage.setItem("saveTasksObject", JSON.stringify(saveTasksObject));
+//$(".saveBtn").on("click", "input", function(){
+//    localStorage.setItem("p", "input");
+//});
+
+//$("#9 .description").val(localStorage.setItem);
+localStorage.setItem('time-block', 'description');
