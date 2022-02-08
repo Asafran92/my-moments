ec0d8abf0456d193aa46042b8639561e4e169278
unknown <avrumiesafranovitz@gmail.com>
2022-02-07 23:46:02 -0500

    fixed JS

--- assets/script.js
+++ assets/script.js
@@ -4,14 +4,14 @@ var currentDay = document.querySelector("#currentDay");
 
 //setting up current day
 var currentDayStart = moment().format('MM-DD-YYYY');
-console.log(currentDayStart);
 
 currentDay = currentDayStart.textContent;
 $("#currentDay").append(currentDayStart);
 $("#currentDay").addClass("lead");
 
-//get row description into local storage
 
+
+//dynamically create hours and time block descriptions
 var container = document.querySelector(".container");
 
 
@@ -19,22 +19,25 @@ for (i = 0; i < hoursOfDay.length; i++) {
     var newDiv = document.createElement("div");
     newDiv.classList.add("row", "time-block");
     newDiv.innerHTML = `<div class="col-md-1 hour">${hoursOfDay[i]}</div>
-    <textarea class="col-md-10 description" id="${hoursOfDay[i]}"></textarea>
+    <textarea class="col-md-10 description" id="${hoursOfDay[i]}">${localStorage.getItem(hoursOfDay[i]) ? localStorage.getItem(hoursOfDay[i]) : ''}</textarea>
     <button class="col-md-1 saveBtn" data-hour="${hoursOfDay[i]}"><i class="fas fa-save"></i></button>`;
     container.appendChild(newDiv);
 }
 
-var arrayOfBtns = document.querySelectorAll('.saveBtn')
-// console.log(arrayOfBtns)
+
+//get row description into local storage
+var arrayOfBtns = document.querySelectorAll('.saveBtn');
 
 arrayOfBtns.forEach(
     function (button) {
         button.addEventListener(
             "click", function () {
-                // console.log(button.dataset.banana)
-                var correspondingText = document.getElementById(button.dataset.hour)
+                var thisHour = button.dataset.hour
+                var correspondingText = document.getElementById(thisHour)
                 console.log(correspondingText.value)
+                localStorage.setItem(thisHour, correspondingText.value)
             }
         )
     }
-)
+);
+
