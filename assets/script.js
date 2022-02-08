945dc6afdee87e28ddc0b06c308b6415bee0ba0f
unknown <avrumiesafranovitz@gmail.com>
2022-02-07 22:03:49 -0500

    added hourly logic

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,10 @@
+hoursOfDay = ['9AM', '10AM', '11AM', '12AM', '1PM', '2PM', '3PM', '4PM', '5PM'];
 var currentDay = document.querySelector("#currentDay");
 
 
 //setting up current day
 var currentDayStart = moment().format('MM-DD-YYYY');
-    console.log(currentDayStart);
+console.log(currentDayStart);
 
 currentDay = currentDayStart.textContent;
 $("#currentDay").append(currentDayStart);
@@ -11,3 +12,29 @@ $("#currentDay").addClass("lead");
 
 //get row description into local storage
 
+var container = document.querySelector(".container");
+
+
+for (i = 0; i < hoursOfDay.length; i++) {
+    var newDiv = document.createElement("div");
+    newDiv.classList.add("row", "time-block");
+    newDiv.innerHTML = `<div class="col-md-1 hour">${hoursOfDay[i]}</div>
+    <textarea class="col-md-10 description" id="${hoursOfDay[i]}"></textarea>
+    <button class="col-md-1 saveBtn" data-hour="${hoursOfDay[i]}"><i class="fas fa-save"></i></button>`;
+    container.appendChild(newDiv);
+}
+
+var arrayOfBtns = document.querySelectorAll('.saveBtn')
+// console.log(arrayOfBtns)
+
+arrayOfBtns.forEach(
+    function (button) {
+        button.addEventListener(
+            "click", function () {
+                // console.log(button.dataset.banana)
+                var correspondingText = document.getElementById(button.dataset.hour)
+                console.log(correspondingText.value)
+            }
+        )
+    }
+)
